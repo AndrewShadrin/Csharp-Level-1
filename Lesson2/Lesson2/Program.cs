f6f12fd5674e7800146f8b00977b7293ab71b05d
AndrewShadrin <andrew_shadrin@mail.ru>
2018-09-22 23:25:15 +0300

    Выполнено 1 задание
    Написана отдельная библиотека класса для дополнительных методов работы на занятиях

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using LessonsClassLibrary;
+
+/// <summary>
+/// 1. Написать метод, возвращающий минимальное из трех чисел.
+/// </summary>
+namespace Task1
+{
+
+    class Program
+    {
+        /// <summary>
+        /// метод возвращает минимальное из трех чисел
+        /// </summary>
+        /// <param name="a">int</param>
+        /// <param name="b">int</param>
+        /// <param name="c">int</param>
+        /// <returns>int</returns>
+        public static int Min3(int a, int b, int c)
+        {
+            int min = a;
+            if (min > b)
+            {
+                min = b;
+            }
+            if (min > c)
+            {
+                min = c;
+            }
+
+            return min;
+        }
+
+        static void Main()
+        {
+            Useful.Print(Min3(3, 2, 1));
+            Useful.Print(Min3(5, 6, 4));
+            Useful.Print(Min3(7, 9, 8));
+            Useful.Print(Min3(10, 11, 12));
+            Useful.Pause(3000);
+        }
+    }
+}
---
+++ LessonsClassLibrary/LessonsClassLibrary/Useful.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+/// <summary>
+/// Библиотека классов для использования на занятиях C# - 1 уровень
+/// </summary>
+namespace LessonsClassLibrary
+{
+    /// <summary>
+    /// Класс представляет методы для быстрого оформления программы
+    /// </summary>
+    public static class Useful
+    {
+        /// <summary>
+        /// Выводит сообщение по указанным координатам. Если не указано, то в текущую позицию курсора.
+        /// </summary>
+        /// <param name="message">строка выводимого сообщения</param>
+        /// <param name="cursX">координата Х позиции курсора</param>
+        /// <param name="cursY">координата Y позиции курсора</param>
+        public static void Print(string message, int cursX = 0, int cursY = 0)
+        {
+            if (cursX!=0 && cursY!=0)
+            {
+                Console.SetCursorPosition(cursX, cursY);
+            }
+            Console.WriteLine(message);
+        }
+
+        /// <summary>
+        /// Выводит сообщение по указанным координатам. Если не указано, то в текущую позицию курсора.
+        /// </summary>
+        /// <param name="message">Значение для вывода</param>
+        /// <param name="cursX">координата Х позиции курсора</param>
+        /// <param name="cursY">координата Y позиции курсора</param>
+        public static void Print(int message, int cursX = 0, int cursY = 0)
+        {
+            if (cursX!=0 && cursY!=0)
+            {
+                Console.SetCursorPosition(cursX, cursY);
+            }
+            Console.WriteLine(message.ToString());
+        }
+
+        /// <summary>
+        /// Осуществляет задержку работы программы, если указано время задержки, или ожидание нажатия клавиши пользователем
+        /// </summary>
+        /// <param name="delay">время задержки в миллисекундах</param>
+        public static void Pause(int delay = 0)
+        {
+            if (delay == 0)
+            {
+                Console.ReadKey();
+            }
+            else
+            {
+                Thread.Sleep(delay);
+            }
+        }
+
+    }
+}
