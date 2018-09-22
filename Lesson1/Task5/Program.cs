76e70f272111e46d8abf4c966547fd9fd6f5055b
AndrewShadrin <andrew_shadrin@mail.ru>
2018-09-22 06:58:37 +0300

    Выполнил 5 задание а,б

---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task5
+{
+    // 5.
+    //  а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+    //  б) Сделать задание, только вывод организуйте в центре экрана
+    //  в) * Сделать задание б с использованием собственных методов(например, Print(string ms, int x, int y)
+    //  Выполнил Шадрин Андрей
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 15, Console.WindowHeight / 2);
+            Console.WriteLine("Андрей Шадрин, Санкт-Петербург");
+            Console.ReadKey();
+        }
+    }
+}
