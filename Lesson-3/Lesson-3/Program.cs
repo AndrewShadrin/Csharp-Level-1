c4eb1bfa8690f4552aedd2a4e65b6c5c63870aba
AndrewShadrin <andrew_shadrin@mail.ru>
2018-09-27 05:39:42 +0300

    Реализованы основные методы работы с дробями
    проверка в основной программе

--- Lesson-3/Lesson-3/Program.cs
+++ Lesson-3/Lesson-3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static LessonsClassLibrary.Useful;
 
 /// <summary>
 /// 3.  *Описать класс дробей - рациональных чисел, являющихся отношением двух целых чисел. 
@@ -24,8 +25,17 @@ namespace Lesson_3
         // denumerator
         private int denum;
 
-        public int Num { get; set; }
-        public int Denum { get; set; }
+        public int Num
+        {
+            get => num;
+            set => num = value;
+        }
+
+        public int Denum
+        {
+            get => denum;
+            set => denum = value;
+        }
 
         public Fraction()
         {
@@ -33,7 +43,7 @@ namespace Lesson_3
             denum = 1;
         }
 
-        public Fraction(int num, int denum)
+        public Fraction(int num, int denum = 1)
         {
             this.num = num;
             this.denum = denum;
@@ -48,8 +58,8 @@ namespace Lesson_3
         {
             Fraction res = new Fraction();
 
-            res.Num = num * fraction.Denum + fraction.Num * denum;
-            res.Denum = denum * fraction.Denum;
+            res.num = num * fraction.denum + fraction.num * denum;
+            res.denum = denum * fraction.denum;
 
             return res;
         }
@@ -93,8 +103,8 @@ namespace Lesson_3
         {
             Fraction res = new Fraction();
 
-            res.Num = num * fraction.Denum;
-            res.Denum = denum * fraction.Num;
+            res.num = num * fraction.denum;
+            res.denum = denum * fraction.num;
 
             return res;
         }
@@ -112,12 +122,43 @@ namespace Lesson_3
 
             return res;
         }
+
+        /// <summary>
+        /// Возвращает строковое представление дроби
+        /// </summary>
+        /// <param name="fraction"></param>
+        /// <returns>строка вида "числитель/знаменатель"</returns>
+        public override string ToString()
+        {
+            return $"{this.num}/{this.denum}";
+        }
     }
 
         class Program
     {
         static void Main()
         {
+            Fraction fraction1, fraction2, result;
+            fraction1 = new Fraction(1,2);
+            fraction2 = new Fraction(1,4);
+            
+            //сложение
+            result = fraction1.Add(fraction2);
+            Print($"{fraction1} + {fraction2} = {result}");
+            
+            //вычитание
+            result = fraction1.Ded(fraction2);
+            Print($"{fraction1} - {fraction2} = {result}");
+            
+            //умножение
+            result = fraction1.Multiply(fraction2);
+            Print($"{fraction1} * {fraction2} = {result}");
+            
+            //деление
+            result = fraction1.Divide(fraction2);
+            Print($"{fraction1} / {fraction2} = {result}");
+
+            Pause();
         }
     }
 }
