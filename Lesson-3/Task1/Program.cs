4a2d4b35ffa25663ff14ef3f4641e62e03c7f89f
AndrewShadrin <andrew_shadrin@mail.ru>
2018-09-30 20:54:34 +0300

    Выполнено 3 задание

---
+++ Lesson-3/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static LessonsClassLibrary.Useful;
+
+/// <summary>
+/// 1.  а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры;
+///     б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса;
+/// Выполнил Шадрин Андрей 
+/// 1. сложение     add,(a, b)+(c, d)=(a+b,c+d);
+/// 2. вычитание    ded,(a, b)-(c, d)=(a-b,c-d);
+/// 3. умножение    mult,(a, b)x(c, d)=(ac-bd,ad+bc);
+/// 4. деление      div,(a, b)/(c, d)=(ac+bd,bc-ad)/(c^2+d^2);
+/// 5. сравнение    equ,(a, b)=(c, d),если(a= c) и(b= d);
+/// </summary>
+namespace Task1
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            // Описали ссылку на объект.
+            Complex complex1;
+            // Создали объект и сохранили ссылку на него в complex1.
+            complex1 = new Complex(1, 1);
+            // Описали объект и создали его.
+            Complex complex2 = new Complex(2, 2);
+            // С помощью свойства Im изменили внутреннее (приватное) поле im.
+            complex2.Im = 3;
+            // Создали ссылку на объект.
+            Complex result;
+            // Так как в методе Plus создается новый объект,
+            // в result сохраняем ссылку на вновь созданный объект.
+            result = complex1.Add(complex2);
+            Console.WriteLine(result.ToString());
+            //вычитание чисел
+            result = complex1.Ded(complex2);
+            Print($"Результат вычитания = {result}");
+            //умножение чисел
+            result = complex1.Mult(complex2);
+            Print($"Результат умножения = {result}");
+            Pause();
+        }
+    }
+}
---
+++ Lesson-3/Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LessonsClassLibrary.Useful;
+
+/// <summary>
+/// 2.  а) С клавиатуры вводятся числа, пока не будет введен 0 (каждое число в новой строке). 
+///        Требуется подсчитать сумму всех нечетных положительных чисел. 
+///        Сами числа и сумму вывести на экран, используя tryParse;
+///     б) Добавить обработку исключительных ситуаций на то, что могут быть введены некорректные данные.
+///        При возникновении ошибки вывести сообщение.Напишите соответствующую функцию;
+/// Выполнил Шадрин Андрей
+/// </summary>
+namespace Task2
+{
+    class Program
+    {
+        static int GetValue(string message)
+        {
+            int x;
+            string s;
+            bool flag; 
+            do
+            {
+                Console.WriteLine(message);
+                s = Console.ReadLine();
+                flag = int.TryParse(s, out x);
+            }
+            while (!flag); 
+            return x;
+        }
+        static void Main()
+        {
+            int number, sum = 0;
+
+            while (true)
+            {
+                number = GetValue("Введите число: ");
+                if (number%2 != 0 && number > 0)
+                {
+                    sum += number;
+                }
+                else if (number == 0)
+                {
+                    break;
+                }
+            }
+            Print($"Сумма нечетных положительных чисел = {sum}");
+            Pause();
+        }
+    }
+}
