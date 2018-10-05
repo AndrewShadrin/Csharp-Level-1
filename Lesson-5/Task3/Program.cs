821f9861910ffb20eb104297f09c5a67973efd9a
AndrewShadrin <andrew_shadrin@mail.ru>
2018-10-05 06:55:11 +0300

    Выполнено 3.б

--- Lesson-5/Task3/Program.cs
+++ Lesson-5/Task3/Program.cs
@@ -15,12 +15,25 @@ namespace Task3
 {
     class Program
     {
+        static bool IsSwap(string s1, string s2)
+        {
+            bool res = true;
 
+            if (s1.Length!=s2.Length)
+            {
+                return false;
+            }
+            s1 = s1.OrderBy(x => x).ToString();
+            s2 = s2.OrderBy(x => x).ToString();
+            res = s1 == s2;
+            return res;
+        }
         static void Main()
         {
             string s1 = ReadString("Введите 1-ю строку: ");
             string s2 = ReadString("Введите 2-ю строку: ");
-            Print(""+s1.OrderBy(x => x).SequenceEqual(s2.OrderBy(x => x)));
+            Print($"Результат сравнения - {s1.OrderBy(x => x).SequenceEqual(s2.OrderBy(x => x))}");
+            Print($"IsSwap = {IsSwap(s1, s2)}");
             Pause();
         }
     }
